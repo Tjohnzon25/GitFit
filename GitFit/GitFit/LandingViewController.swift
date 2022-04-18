2d3249a4e6364c43371cf1ade4a262236d3a9f77
Timothy Johnson <60270341+Tjohnzon25@users.noreply.github.com>
2022-04-18 11:05:19 -0700

    Created project flow and added Parse

---
+++ GitFit/GitFit/LandingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LandingViewController.swift
+//  GitFit
+//
+//  Created by Tim Johnson on 4/18/22.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func onLogin(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+    }
+    
+}
