53a800dda98f0f9d1ab357b5cc2cca5adcb2e7e0
Timothy Johnson <60270341+Tjohnzon25@users.noreply.github.com>
2022-04-18 17:17:28 -0700

    initializes MyWorkout and Weekly scheduling to empty upon creation of user

--- GitFit/GitFit/LandingViewController.swift
+++ GitFit/GitFit/LandingViewController.swift
@@ -22,7 +22,7 @@ class LandingViewController: UIViewController {
         
         let username = usernameTextField.text!
         let password = passwordTextField.text!
-                
+        
         PFUser.logInWithUsername(inBackground: username, password: password) { user, error in
             if user != nil {
                 self.performSegue(withIdentifier: "landingToMain", sender: nil)
@@ -31,21 +31,54 @@ class LandingViewController: UIViewController {
             }
         }
     }
-
-
+    
+    
     @IBAction func onSignUp(_ sender: Any) {
         
         let user = PFUser()
         user.username = usernameTextField.text
         user.password = passwordTextField.text
-                
+        
         user.signUpInBackground{ (success, error) in
             if success {
-                self.performSegue(withIdentifier: "landingToMain", sender: nil)
-            } else {
-                print("ERROR: \(String(describing: error?.localizedDescription))")
+                
+                let MyWorkouts = PFObject(className: "MyWorkouts")
+                MyWorkouts["Arms"] = []
+                MyWorkouts["Chest"] = []
+                MyWorkouts["Back"] = []
+                MyWorkouts["Legs"] = []
+                MyWorkouts["Biceps"] = []
+                MyWorkouts["Triceps"] = []
+                MyWorkouts["User"] = PFUser.current()!
+                
+                MyWorkouts.saveInBackground {
+                    (success: Bool, error: Error?) in
+                    if (success) {
+                        
+                        let schedule = PFObject(className: "Schedule")
+                        schedule["Monday"] = []
+                        schedule["Tuesday"] = []
+                        schedule["Wednesday"] = []
+                        schedule["Thursday"] = []
+                        schedule["Friday"] = []
+                        schedule["Saturday"] = []
+                        schedule["Sunday"] = []
+                        schedule["User"] = PFUser.current()!
+                        
+                        schedule.saveInBackground {
+                            (success: Bool, error: Error?) in
+                            if (success) {
+                                self.performSegue(withIdentifier: "landingToMain", sender: nil)
+                            } else {
+                                print("ERROR: \(String(describing: error))")
+                            }
+                        }
+                        
+                    } else {
+                        print("ERROR: \(String(describing: error?.localizedDescription))")
+                    }
+                }
             }
         }
     }
-    
 }
